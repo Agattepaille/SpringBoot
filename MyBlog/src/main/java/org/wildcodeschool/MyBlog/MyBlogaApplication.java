e9e10fc2aeb0fa7ed45a55402848f56bcfd358c2
Agattepaille <amelie.gattepaille@hotmail.fr>
2025-01-13 14:39:17 +0100

    quete 1

---
+++ MyBlog/src/main/java/org/wildcodeschool/MyBlog/MyBlogaApplication.java
@@ -0,0 +1,13 @@
+package org.wildcodeschool.MyBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogaApplication.class, args);
+	}
+
+}
