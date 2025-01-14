1d3f9eca829caef7c1dfe87c06c899face95412b
Agattepaille <amelie.gattepaille@hotmail.fr>
2025-01-14 14:50:15 +0100

    ok

--- MyBlog/src/main/java/org/wildcodeschool/MyBlog/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package org.wildcodeschool.MyBlog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.wildcodeschool.MyBlog.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
